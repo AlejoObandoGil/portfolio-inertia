6231e9d651d10e35c226c550bfe1a691af5c96f1
Alejandro Obando Gil <alejo.obando.g@gmail.com>
2021-12-26 23:11:57 -0500

    first commit

---
+++ resources/js/Pages/User.vue
@@ -0,0 +1,29 @@
+<template>
+    <app-layout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Users
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <p v-for="(user, index) in users" :key="index">{{ user.name }}</p>
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import { defineComponent } from 'vue'
+    import AppLayout from '@/Layouts/AppLayout.vue'
+
+    export default defineComponent({
+        components: {
+            AppLayout,
+        },
+        props:['users']
+    })
+</script>
